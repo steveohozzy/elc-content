7ffcccb80e2f5e84d225dbce2512aef1f0f11d29
steveoHozzy <steve.hozzy@gmail.com>
2026-06-08 14:59:18 +0100

    adjusting hero so fetch etc works correctly with next

--- components/sections/HomeHero.tsx
+++ components/sections/HomeHero.tsx
@@ -29,20 +29,21 @@ export default function HomeHeroSection({
 
         {/* BACKGROUND IMAGE */}
         <div className="absolute inset-0">
-          <Image
-            src={
+        <Image
+          fill
+          priority
+          fetchPriority="high"
+          sizes="(min-width: 1280px) 1280px, 100vw"
+          src={
               data?.backgroundImage?.url
                 ? `${data?.backgroundImage.url}`
                 : "https://images.unsplash.com/photo-1590461283969-47fedf408cfd?fm=jpg&q=60&w=3000&auto=format&fit=crop"
             }
             alt={data?.backgroundImage?.title || "Hero image"}
-            width={1920}
-            height={1080}
-            priority
-            className="h-full w-full object-cover opacity-60"
-            quality={75}
-          />
-        </div>
+          className="object-cover opacity-60"
+          quality={75}
+        />
+      </div>
 
         {/* OVERLAY */}
         <div className="absolute inset-0 bg-gradient-to-b from-black/30 via-black/60 to-black" />
